5fb3900228f156443ae820c42c8e4fbd194ad623
etiry <7997357+etiry@users.noreply.github.com>
2023-08-01 12:58:14 -0400

    Add upvote and downvote icons

--- main.js
+++ main.js
@@ -13,10 +13,14 @@ function addPost () {
   messageDiv.innerHTML = messageInput.value;
 
   const postedByDiv = document.createElement('div')
-  postedByDiv.innerHTML = `Posted By: <strong>${nameInput.value}</strong><hr class="hr" />`;
+  postedByDiv.innerHTML = `Posted By: <strong>${nameInput.value}</strong>`;
+
+  const voteDiv = document.createElement('div');
+  voteDiv.innerHTML = '<i class="fa-solid fa-thumbs-up"></i> <i class="fa-solid fa-thumbs-down"></i> <hr class="hr" />'
 
   postDiv.appendChild(messageDiv);
   postDiv.appendChild(postedByDiv);
+  postDiv.appendChild(voteDiv);
 
   postsDiv.appendChild(postDiv);
 
