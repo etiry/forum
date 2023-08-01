9db43618745ca10829df1dd1b71e1b29ba6a618c
etiry <7997357+etiry@users.noreply.github.com>
2023-08-01 14:16:27 -0400

    Add count upvote and downvote functions

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
+const Post = (name, message) => {
+  const upvotes = 0;
+  const downvotes = 0;
+  const index = null;
+
+  return {
+    name,
+    message,
+    upvotes,
+    downvotes,
+    index
+  }
+}
+
+const postList = () => {
+  const posts = [];
+
+  const addPostToList = (post) => {
+    posts.push(post);
+    post.index = posts.indexOf(post);
+  }
+
+  return {
+    posts, 
+    addPostToList
+  }
+}
+
+myPosts = postList();
+
 const postsDiv = document.querySelector('.posts');
 const nameInput = document.querySelector('#name');
 const messageInput = document.querySelector('#message');
 const submitButton = document.querySelector('#submit');
 
+
 submitButton.addEventListener('click', addPost);
 
+function createNewPost (name, message) {
+  return Post(name, message);
+}
 
 function addPost () {
+  const newPost = createNewPost(nameInput.value, messageInput.value);
+
+  myPosts.addPostToList(newPost);
+
   const postDiv = document.createElement('div');
+  postDiv.setAttribute('data-index', newPost.index);
 
   const messageDiv = document.createElement('div')
-  messageDiv.innerHTML = messageInput.value;
+  messageDiv.innerHTML = newPost.message;
 
   const postedByDiv = document.createElement('div')
-  postedByDiv.innerHTML = `Posted By: <strong>${nameInput.value}</strong>`;
+  postedByDiv.innerHTML = `Posted By: <strong>${newPost.name}</strong>`;
 
   const voteDiv = document.createElement('div');
   voteDiv.innerHTML = '<i class="fa-solid fa-thumbs-up"></i> <i class="fa-solid fa-thumbs-down"></i> <hr class="hr" />'
@@ -26,4 +65,22 @@ function addPost () {
 
   messageInput.value = '';
   nameInput.value = '';
+
+  const upvote = document.querySelector('.fa-thumbs-up');
+  const downvote = document.querySelector('.fa-thumbs-down');
+
+  upvote.addEventListener('click', countUpvote);
+  downvote.addEventListener('click', countDownvote);
+}
+
+function countUpvote (e) {
+  const postIndex = e.target.parentElement.parentElement.dataset.index;
+
+  myPosts.posts[postIndex].upvotes += 1;
+}
+
+function countDownvote (e) {
+  const postIndex = e.target.parentElement.parentElement.dataset.index;
+
+  myPosts.posts[postIndex].downvotes += 1;
 }
